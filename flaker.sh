79f69f56b0700a03427f1b5e991548f55bd130cf
Daniel Prince <danielahprince@live.com>
2019-09-23 18:54:22 +0200

    added some comments

--- flaker.sh
+++ flaker.sh
@@ -3,6 +3,7 @@
 URL=${1?Error: No url given}
 BRANCH=${2:-main}
 
+# appends .git if url is provided
 if [[ $URL =~ \.git$ ]]
 then
     echo "is a git repo"
@@ -12,12 +13,17 @@ else
     echo "changed to: ${URL}"
 fi
 
+# clones repo into temp directory
 git clone $URL -q -b $BRANCH ./tmp/
 cd ./tmp
+
+# gets a hold of files that have changed, and includes only the python files
 DIRS=$(git diff --name-only --summary origin/master..origin/${BRANCH} | grep .py)
 echo "following files included:"
 echo $(echo $DIRS | sed -E "s/\s[a-z0-9\/\_]*\//, /g")
 echo -e '\n\n'
+
+#runs flake8 on those
 flake8 $DIRS
 cd ../
 rm -Rf ./tmp
