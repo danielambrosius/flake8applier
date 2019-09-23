2892c45b230f4cc5313b4df732957967e0e92257
Daniel Prince <danielahprince@live.com>
2019-09-23 17:22:26 +0200

    fixed first input

--- flaker.sh
+++ flaker.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
 
+URL=${1?Error: No url given}
+BRANCH=${2:-main}
+
+if [[ $URL =~ \.git$ ]]
+then
+    echo "is a git repo"
+else
+    echo "appears to be url, appending .git"
+    URL+=".git"
+    echo "changed to: ${URL}"
+fi
